2b38d401beed6ad4936cd42ddd2d57bc49491df8
James Hinton <james.hinton@live.co.uk>
2023-03-04 11:21:39 +0000

    refactor: removed unneeded code

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.scss';
 
 const Popup = () => {
   return (
     <div className="popup">
       <header className="popup-header">
-        <img src={logo} className="popup-logo" alt="logo" />
+        <h3>Code Stash</h3>
       </header>
     </div>
   );
