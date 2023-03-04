5aa0d5291c54154742343043526a3984370c9801
James Hinton <james.hinton@live.co.uk>
2023-03-04 12:18:05 +0000

    feat: added custom stack overflow search

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -5,11 +5,29 @@ const Popup = () => {
   const [code, setCode] = useState([]);
 
   function searchCode() {
-    chrome.runtime.sendMessage({ type: 'LOG_MESSAGE', message: 'Searching for code' });
-  
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
+      const url = new URL(tabs[0].url);
+      let searchFunction = null;
+
+      if (url.hostname === 'stackoverflow.com') {
+        searchFunction = searchStackOverflowCode;
+      } else {
+        searchFunction = searchBasicCode;
+      }
+
+      searchFunction(tabs[0].id);
+    });
+  }
+
+  function searchBasicCode(tabId) {
+    chrome.runtime.sendMessage({
+      type: 'LOG_MESSAGE',
+      message: 'Searching Basic!',
+    });
+
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
         function: function () {
           const codeBlocks = document.getElementsByTagName('code');
           const codeTexts = [];
@@ -19,19 +37,43 @@ const Popup = () => {
           }
           return codeTexts;
         },
-      }).then(function (result) {
-        console.log('Code blocks:', result[0]);
-        chrome.runtime.sendMessage({ type: 'LOG_MESSAGE', message: 'Code blocks found' });
-        chrome.runtime.sendMessage({ type: 'LOG_MESSAGE', message: result[0].result });
+      })
+      .then(function (result) {
+        chrome.runtime.sendMessage({ type: 'LOG_MESSAGE', message: result });
       });
-    });
   }
-  
-  
 
-  const handleClick = () => {
-    chrome.runtime.sendMessage({ type: 'LOG_MESSAGE', message: 'Testing the button' });
-  };
+  function searchStackOverflowCode(tabId) {
+    chrome.runtime.sendMessage({
+      type: 'LOG_MESSAGE',
+      message: 'Running on stack overflow',
+    });
+
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
+        function: function () {
+          const codeBlocks = document.querySelectorAll('.s-code-block');
+          const codeTexts = [];
+          for (let i = 0; i < codeBlocks.length; i++) {
+            codeBlocks[i].style.border = '3px solid blue';
+            const codeElement = codeBlocks[i].getElementsByTagName('code')[0];
+            const language = 'unknown'
+            codeTexts.push({
+              language,
+              code: codeElement.innerText,
+            });
+          }
+          return codeTexts;
+        },
+      })
+      .then(function (result) {
+        chrome.runtime.sendMessage({
+          type: 'LOG_MESSAGE',
+          message: result[0].result,
+        });
+      });
+  }
 
   return (
     <div className="popup">
@@ -41,8 +83,6 @@ const Popup = () => {
       <div className="popup-content">
         <p>Code found on this pagesss</p>
         <button onClick={searchCode}>Log to console</button>
-
-
       </div>
     </div>
   );
