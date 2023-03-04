430235ba681a309294cdbbdedd058bcbd8cfad24
James Hinton <james.hinton@live.co.uk>
2023-03-04 11:53:04 +0000

    refactor: corrected formatting on background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === 'LOG_MESSAGE') {
-      console.log(message.message);
-    }
-  });
+  if (message.type === 'LOG_MESSAGE') {
+    console.log(message.message);
+  }
+});
